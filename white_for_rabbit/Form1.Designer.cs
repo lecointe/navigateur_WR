2299172222db008a68d9275e23de95766caaa3c1
lecointe <r.fighter@hotmail.fr>
2015-04-30 08:51:11 +0200

    MyBrow

---
+++ white_for_rabbit/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace white_for_rabbit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.recherche = new MetroFramework.Controls.MetroTextBox();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.ButtonTraducteur = new MetroFramework.Controls.MetroTile();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.ButtonBack = new MetroFramework.Controls.MetroButton();
+            this.ButtonNext = new MetroFramework.Controls.MetroButton();
+            this.ButtonActu = new MetroFramework.Controls.MetroButton();
+            this.ButtonStop = new MetroFramework.Controls.MetroButton();
+            this.ButtonHome = new MetroFramework.Controls.MetroButton();
+            this.url = new System.Windows.Forms.ComboBox();
+            this.test = new MetroFramework.Controls.MetroComboBox();
+            this.metroPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.metroTabControl1.Location = new System.Drawing.Point(20, 60);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.Size = new System.Drawing.Size(1111, 524);
+            this.metroTabControl1.TabIndex = 0;
+            this.metroTabControl1.UseSelectable = true;
+            this.metroTabControl1.Click += new System.EventHandler(this.metroTabControl1_Click);
+            // 
+            // recherche
+            // 
+            this.recherche.Lines = new string[0];
+            this.recherche.Location = new System.Drawing.Point(17, 3);
+            this.recherche.MaxLength = 32767;
+            this.recherche.Name = "recherche";
+            this.recherche.PasswordChar = '\0';
+            this.recherche.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.recherche.SelectedText = "";
+            this.recherche.Size = new System.Drawing.Size(158, 23);
+            this.recherche.TabIndex = 3;
+            this.recherche.UseSelectable = true;
+            this.recherche.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.recherche_KeyPress);
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.BackColor = System.Drawing.Color.Lime;
+            this.metroPanel1.Controls.Add(this.ButtonTraducteur);
+            this.metroPanel1.Controls.Add(this.recherche);
+            this.metroPanel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(953, 5);
+            this.metroPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(200, 600);
+            this.metroPanel1.TabIndex = 2;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            this.metroPanel1.MouseLeave += new System.EventHandler(this.metroPanel1_MouseLeave);
+            this.metroPanel1.MouseHover += new System.EventHandler(this.metroPanel1_MouseHover);
+            this.metroPanel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.metroPanel1_MouseMove);
+            // 
+            // ButtonTraducteur
+            // 
+            this.ButtonTraducteur.ActiveControl = null;
+            this.ButtonTraducteur.Location = new System.Drawing.Point(41, 35);
+            this.ButtonTraducteur.Name = "ButtonTraducteur";
+            this.ButtonTraducteur.Size = new System.Drawing.Size(75, 23);
+            this.ButtonTraducteur.TabIndex = 4;
+            this.ButtonTraducteur.Text = "traduire";
+            this.ButtonTraducteur.UseSelectable = true;
+            this.ButtonTraducteur.Click += new System.EventHandler(this.ButtonTraducteur_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 40;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroTile1.Location = new System.Drawing.Point(1143, 40);
+            this.metroTile1.Margin = new System.Windows.Forms.Padding(0);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(10, 544);
+            this.metroTile1.TabIndex = 3;
+            this.metroTile1.UseSelectable = true;
+            this.metroTile1.MouseEnter += new System.EventHandler(this.metroTile1_MouseEnter);
+            this.metroTile1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.metroTile1_MouseMove);
+            // 
+            // ButtonBack
+            // 
+            this.ButtonBack.FontWeight = MetroFramework.MetroButtonWeight.Regular;
+            this.ButtonBack.Location = new System.Drawing.Point(0, 40);
+            this.ButtonBack.Name = "ButtonBack";
+            this.ButtonBack.Size = new System.Drawing.Size(75, 23);
+            this.ButtonBack.TabIndex = 4;
+            this.ButtonBack.Text = "back";
+            this.ButtonBack.UseSelectable = true;
+            this.ButtonBack.Click += new System.EventHandler(this.ButtonBack_Click);
+            // 
+            // ButtonNext
+            // 
+            this.ButtonNext.Location = new System.Drawing.Point(81, 40);
+            this.ButtonNext.Name = "ButtonNext";
+            this.ButtonNext.Size = new System.Drawing.Size(75, 23);
+            this.ButtonNext.TabIndex = 5;
+            this.ButtonNext.Text = "next";
+            this.ButtonNext.UseSelectable = true;
+            this.ButtonNext.Click += new System.EventHandler(this.ButtonNext_Click);
+            // 
+            // ButtonActu
+            // 
+            this.ButtonActu.Location = new System.Drawing.Point(162, 40);
+            this.ButtonActu.Name = "ButtonActu";
+            this.ButtonActu.Size = new System.Drawing.Size(80, 23);
+            this.ButtonActu.TabIndex = 6;
+            this.ButtonActu.Text = "Actu";
+            this.ButtonActu.UseSelectable = true;
+            this.ButtonActu.Click += new System.EventHandler(this.ButtonActu_Click);
+            // 
+            // ButtonStop
+            // 
+            this.ButtonStop.Location = new System.Drawing.Point(248, 40);
+            this.ButtonStop.Name = "ButtonStop";
+            this.ButtonStop.Size = new System.Drawing.Size(75, 23);
+            this.ButtonStop.TabIndex = 7;
+            this.ButtonStop.Text = "Stop";
+            this.ButtonStop.UseSelectable = true;
+            this.ButtonStop.Click += new System.EventHandler(this.ButtonStop_Click);
+            // 
+            // ButtonHome
+            // 
+            this.ButtonHome.Location = new System.Drawing.Point(329, 40);
+            this.ButtonHome.Name = "ButtonHome";
+            this.ButtonHome.Size = new System.Drawing.Size(75, 23);
+            this.ButtonHome.TabIndex = 8;
+            this.ButtonHome.Text = "Home";
+            this.ButtonHome.UseSelectable = true;
+            this.ButtonHome.Click += new System.EventHandler(this.ButtonHome_Click);
+            // 
+            // url
+            // 
+            this.url.FormattingEnabled = true;
+            this.url.Location = new System.Drawing.Point(0, 16);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(564, 21);
+            this.url.TabIndex = 9;
+            this.url.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.url_KeyPress);
+            // 
+            // test
+            // 
+            this.test.FormattingEnabled = true;
+            this.test.ItemHeight = 23;
+            this.test.Location = new System.Drawing.Point(582, 16);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(368, 29);
+            this.test.TabIndex = 1;
+            this.test.UseSelectable = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1151, 604);
+            this.Controls.Add(this.url);
+            this.Controls.Add(this.metroTile1);
+            this.Controls.Add(this.test);
+            this.Controls.Add(this.ButtonHome);
+            this.Controls.Add(this.ButtonStop);
+            this.Controls.Add(this.ButtonActu);
+            this.Controls.Add(this.ButtonNext);
+            this.Controls.Add(this.ButtonBack);
+            this.Controls.Add(this.metroPanel1);
+            this.Controls.Add(this.metroTabControl1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.metroPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private MetroFramework.Controls.MetroTextBox recherche;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Controls.MetroTile ButtonTraducteur;
+        private MetroFramework.Controls.MetroButton ButtonBack;
+        private MetroFramework.Controls.MetroButton ButtonNext;
+        private MetroFramework.Controls.MetroButton ButtonActu;
+        private MetroFramework.Controls.MetroButton ButtonStop;
+        private MetroFramework.Controls.MetroButton ButtonHome;
+        private System.Windows.Forms.ComboBox url;
+        private MetroFramework.Controls.MetroComboBox test;
+    }
+}
+
---
+++ white_for_rabbit/Form1.cs
@@ -0,0 +1,414 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace white_for_rabbit
+{
+
+
+
+    public partial class Form1 : MetroForm
+    {
+                //Awesomium.Windows.Forms.WebControl Brow;
+        MyBrow Brow;
+        string Home = "http://devlog.amorce.org/#";
+        string moteur = "https://www.google.fr/#q=";
+        int type = 2;
+        int i=0;
+           
+
+ #region fonction
+
+       void uurl(int type)
+        {
+            if (type == 1)
+            {
+
+              /* 
+                if (url.Text.StartsWith("http://") || url.Text.StartsWith("https://") || url.Text.EndsWith(".com") || url.Text.EndsWith(".fr") == true)
+                {
+                    if (url.Text.StartsWith("http://") || url.Text.StartsWith("https://") == true)
+                    {
+                        Browser.Source = new Uri(url.Text);
+                        // si le texte tapé comence par http:// ou https:// on va a l'adresse tapée
+
+                    }
+
+                    if (url.Text.EndsWith(".com") == true)
+                    {
+                        Browser.Source = new Uri("https://" + url.Text);
+                        // si le texte tapé fini par .com on va a l'adresse tapée
+
+                    }
+
+                    if (url.Text.EndsWith(".fr") == true)
+                    {
+                        Browser.Source = new Uri("http://" + url.Text);
+                        // si le texte tapé fini par .fr on va a l'adresse tapée
+
+                    }
+
+                }
+                else
+                {
+                    Browser.Source = new Uri(moteur + url.Text);
+
+                    //Sinon on le recherche
+                }
+                */
+            }
+            if (type == 2)
+            {
+                if (url.Text.StartsWith("http://") || url.Text.StartsWith("https://") || url.Text.EndsWith(".com") || url.Text.EndsWith(".fr") == true)
+                {
+                    if (url.Text.StartsWith("http://") || url.Text.StartsWith("https://") == true)
+                    {
+                        ((WebBrowser)metroTabControl1.SelectedTab.Controls[0]).Navigate(new Uri(url.Text));
+                        // si le texte tapé comence par http:// ou https:// on va a l'adresse saisie
+                    }
+                    if (url.Text.EndsWith(".com") == true)
+                    {
+                        ((WebBrowser)metroTabControl1.SelectedTab.Controls[0]).Navigate(new Uri("https://" + url.Text));
+                        // si le texte tapé fini par .com on va a l'adresse saisie
+                    }
+                    if (url.Text.EndsWith(".fr") == true)
+                    {
+                        ((WebBrowser)metroTabControl1.SelectedTab.Controls[0]).Navigate(new Uri("http://" + url.Text));
+                        // si le texte tapé fini par .fr on va a l'adresse saisie
+                    }
+                }
+                else
+                {
+                    ((WebBrowser)metroTabControl1.SelectedTab.Controls[0]).Navigate(new Uri(moteur + url.Text));
+                    //Sinon on le recherche
+                }
+            }
+        }
+        void back(int type)
+        {
+            if (type == 1)
+            {
+                /*
+                if (Browser.CanGoBack() == true) // Si l'on peut revenir en arrière ...
+                {
+                    Browser.GoBack(); // ... on revient en arrière.
+                }
+                else
+                {
+                    MessageBox.Show("Impossible de revenir en arrière"); // Sinon on affiche un message d'erreur.
+                }*/
+            }
+            if (type == 2)
+            {
+                
+                    ((WebBrowser)metroTabControl1.SelectedTab.Controls[0]).GoBack(); // ... on revient en arrière.
+             
+                
+            }
+        }
+        void nnext(int type)
+        {
+            if (type == 1)
+            {
+                /*
+                if (Browser.CanGoForward() == true) // Si on peut revenir en avant ...
+                {
+                    Browser.GoForward(); // ... on revient en avant.
+                }
+                else
+                {
+                    MessageBox.Show("Impossible de revenir en avant"); // Sinon on affiche un message d'erreur.
+                }*/
+            }
+            if (type == 2)
+            {
+                ((WebBrowser)metroTabControl1.SelectedTab.Controls[0]).GoForward();
+            }
+        }
+        void aactu(int type)
+        {
+            if (type == 1)
+            {
+               // Browser.Reload(true);
+            }
+            if (type == 2)
+            {
+                ((WebBrowser)metroTabControl1.SelectedTab.Controls[0]).Refresh();
+            }
+        }
+        void sstop(int type)
+        {
+            if (type == 1)
+            {
+             //   Browser.Stop();
+            }
+            if (type == 2)
+            {
+                Brow.Stop();
+            }
+        }
+        void hhome(int type)
+        {
+
+            if (type == 1)
+            {
+               // Browser.Source = new Uri(Home);
+            }
+            if (type == 2)
+            {
+                ((WebBrowser)metroTabControl1.SelectedTab.Controls[0]).Navigate(Home);
+            }
+        }
+        void rrecherche(int type)
+        {
+            if (type == 1)
+            {
+
+                //Browser.Source = new Uri(moteur + recherche.Text);
+
+            }
+            if (type == 2)
+            {
+                ((WebBrowser)metroTabControl1.SelectedTab.Controls[0]).Navigate(new Uri(moteur + recherche.Text));
+            }
+        }
+       private void addNewTab()
+        {
+           
+        //Creation d'un nouvel objet TabPage
+            TabPage tpage = new TabPage();
+
+        //Ajoute l'onglet nouvellement créer à la collection de controle d'onglet
+            metroTabControl1.TabPages.Insert(metroTabControl1.TabCount - 1, tpage);
+
+        //Creation d'un objet navigateur
+           Brow = new MyBrow(metroTabControl1,url,tpage);
+          
+        //Ajoute le navigateur a l'onglet précedement créer
+            tpage.Controls.Add(Brow);
+            Brow.Dock = DockStyle.Fill;
+            Brow.ScriptErrorsSuppressed = true;
+            metroTabControl1.SelectTab(tpage);
+
+            Brow.Navigate(moteur);    
+          
+        //Suivre l'onglet new tab 
+            i++;
+  
+        }
+
+
+
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Initialisation du menu droit
+            metroPanel1.Width = 100;
+            metroPanel1.Dock = DockStyle.Right;
+            metroPanel1.Visible = false;
+            metroPanel1.BackColor = Color.Black;
+           
+            //Gestion des onglets, ancrage au 4 coins, onglet vide nommé "New Tab", incremente nombre onglet avec i
+            metroTabControl1.TabPages.Add("New Tab");
+            metroTabControl1.SelectTab(i);
+            metroTabControl1.SelectedTab.Controls.Add(Brow);
+            metroTabControl1.Dock = DockStyle.Fill;
+     
+            addNewTab();
+  
+             
+            //Les boutons precedent, suivant et stop sont grisé au demarage
+            // buttonBack.Enabled = false;
+            // buttonForward.Enabled = false;
+            // buttonStop.Enabled = false;
+            
+        }     
+
+
+#region menu droit
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.metroPanel1.Size.Width >= 209) this.timer1.Enabled = false;
+            else this.metroPanel1.Width += 15;
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (this.metroPanel1.Size.Width <= 10) this.timer2.Enabled = false;
+            else this.metroPanel1.Width -= 15;
+            if (metroPanel1.Width <= 10) metroPanel1.Visible = false;
+        }
+        private void metroPanel1_MouseLeave(object sender, EventArgs e)
+        {
+ 
+        }
+        private void metroTile1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //le menu arrive 
+            metroPanel1.Visible = true;
+            this.timer2.Enabled = false;
+            this.timer1.Enabled = true;
+        }
+        private void metroPanel1_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+        private void metroPanel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.X <=5)//le menu part [e.X est la pos du curseur par rapport au menu]
+            {
+                this.timer2.Enabled = true;
+                this.timer1.Enabled = false;
+            }
+            if (e.X > 10)// le menu revient
+            {
+                this.timer2.Enabled = false;
+                this.timer1.Enabled = true;
+            }
+            
+        }
+        private void metroTile1_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+
+#region boutton
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            sstop(type);
+        }
+        private void metroTabControl1_Click(object sender, EventArgs e)
+        {
+            if (metroTabControl1.SelectedIndex == i)
+            {
+                addNewTab();
+            }
+        }
+        private void ButtonHome_Click(object sender, EventArgs e)
+        {
+            hhome(type);
+        }
+        private void ButtonBack_Click(object sender, EventArgs e)
+        {
+            Brow.back();
+        }
+        private void url_KeyPress(object sender, KeyPressEventArgs e)
+        {
+               if (e.KeyChar == (char)13)
+            {
+                Brow.url();
+            }
+            
+        }
+        private void ButtonActu_Click(object sender, EventArgs e)
+        {
+            aactu(type); 
+            
+        }
+        private void ButtonNext_Click(object sender, EventArgs e)
+        {
+            nnext(type); 
+        }
+        private void recherche_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                rrecherche(type);
+            }
+           
+        }
+        private void ButtonTraducteur_Click(object sender, EventArgs e)
+        {
+            // ne fonctionne pas pour webbrowser mais pour awesonium
+            // Brow.ExecuteJavascript("(function(){var s = document.createElement('script'); s.type = 'text/javascript'; s.src = 'http://labs.microsofttranslator.com/bookmarklet/default.aspx?f=js&to=fr'; document.body.insertBefore(s, document.body.firstChild);})()");
+        }
+#endregion
+    }
+
+#region MyBrow
+    class MyBrow : WebBrowser
+    {
+        public string moteur = "https://www.google.fr/#q=";
+        private string _nom;
+        private TabPage _tpage;
+        private MetroFramework.Controls.MetroTabControl _metroTabControl;
+        private System.Windows.Forms.ComboBox _combo;
+        public MyBrow(MetroFramework.Controls.MetroTabControl metroTabControl, System.Windows.Forms.ComboBox combo, TabPage tpage)
+        {
+            _metroTabControl=metroTabControl;
+            _combo=combo;
+            _tpage = tpage;
+         //lier la fontion Brow_DocumentCompleted a l'objet 
+            this.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(Brow_DocumentCompleted);
+         //   this.DocumentCompleted += new WebBrowserDocumentCompletedEventHandle(r);
+          
+        }
+        private void Brow_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            string result;
+            int L;
+     
+            this._nom = this.DocumentTitle;
+            L = _nom.Length;
+            result = "";
+            if (L > 30)                                              //si nom > a la longueur -> mettre la longueur voulu dans L + ... 
+            {
+                L = 30;
+                result = "...";
+            }
+            result = _nom.Substring(0, L) + result;                 // couper la chaine apres la longueur L                      
+            _tpage.Text=result;
+           // si l'onglet de l'objet est l'onglet actuel afficher l'url 
+              if (_metroTabControl.SelectedTab ==  _tpage)_combo.Text =this.Url.ToString();
+              
+            }
+        public void url()
+        {
+                if ( _combo.Text.StartsWith("http://") ||  _combo.Text.StartsWith("https://") ||  _combo.Text.EndsWith(".com") ||  _combo.Text.EndsWith(".fr") == true)
+                {
+                    if ( _combo.Text.StartsWith("http://") || _combo.Text.StartsWith("https://") == true)
+                    {
+                        this.Navigate(new Uri( _combo.Text));
+                        // si le texte tapé comence par http:// ou https:// on va a l'adresse saisie
+                    }
+                    if ( _combo.Text.EndsWith(".com") == true)
+                    {
+                        this.Navigate(new Uri("https://" + _combo.Text));
+                        // si le texte tapé fini par .com on va a l'adresse saisie
+                    }
+                    if ( _combo.Text.EndsWith(".fr") == true)
+                    {
+                        this.Navigate(new Uri("http://" +  _combo.Text));
+                        // si le texte tapé fini par .fr on va a l'adresse saisie
+                    }
+                }
+                else
+                {
+                    this.Navigate(new Uri(moteur +  _combo.Text));
+                    //Sinon on le recherche
+                }
+        }
+        public void back()
+        {
+                this.GoBack(); // ... on revient en arrière.
+        }
+     }
+
+    #endregion
+
+}
